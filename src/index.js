c24c0f96a73952a7f4563827ad7c33996ecba3aa
Mavludin <imevlud@gmail.com>
2020-12-16 11:57:24 +0300

    tried to solve the routing issue

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-<BrowserRouter basename="/portfolio-page">
+<BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
